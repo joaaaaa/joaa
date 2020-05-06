3928d49d9bb5e9db7c8455af488d521fcae99f88
sysoev_kirill <sysoev01@bk.ru>
2020-05-06 18:06:00 +0300

    lab4.0

--- index.php
+++ index.php
@@ -7,3 +7,21 @@ include_once 'joaa/Linear.php';
 include_once 'joaa/Square.php';
 include_once 'joaa/joaaException.php';
 include_once 'joaa/Log.php';
+
+$arr=array();
+
+$arr[] = readline("a= ");
+$arr[] = readline("b= ");
+$arr[] = readline("c= ");
+
+try {
+    $solver = new Alex\Square();
+	$roots = $solver->solve($arr[0], $arr[1], $arr[2]);
+
+    Alex\Log::log("roots: " . implode(" , ", $roots));
+   
+}catch(Alex\joaaException $e) {
+
+    Alex\Log::log($e->getMessage());
+}
+Alex\Log::write();
